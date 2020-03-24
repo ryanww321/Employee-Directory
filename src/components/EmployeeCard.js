53dd5efbada66c066d88458eb9b6ec039066c770
Ryan Whitcomb <ryanww321@gmail.com>
2020-03-24 18:30:04 -0500

    added some css to the employeecard and header files

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -5,9 +5,11 @@ import React from "react";
 function EmployeeCard(props){
     return (
     <div>
-        <div className="employeeCard">
+        <div className="employeeCard ml-2">
             <img src={props.src} alt={props.firstName}></img>
-            <p>{props.firstName} {props.lastName} {props.email} {props.phone}</p>
+            <p>{props.firstName} {props.lastName}</p>
+            <p>{props.email}</p>
+            <p>{props.phone}</p>
         </div>
         <hr></hr>
     </div>
